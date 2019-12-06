8a8f0d95eb3265969007779930eeabc2b27d4ce2
Brandon Allison <brandon.james.allison@gmail.com>
2019-12-06 13:10:44 -0500

    Started stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -89,6 +89,12 @@ for c in cities:
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
+
+  if lat1 > lat2:
+  else:
+  if lon1 > lon2:
+  else:
+     
   within = []
 
   # TODO Ensure that the lat and lon valuse are all floats
